6e7a9897eb3207981684d610e0ac69a9bab81b81
iutkarshmehta <utkarshmehta4991@gmail.com>
2024-10-17 13:25:55 +0530

    INR-USD conversion app using storyboard

--- currency-convertor-basic-storyboard/ViewController.swift
+++ currency-convertor-basic-storyboard/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var rateTextField: UITextField!
+    @IBOutlet weak var amountTextField: UITextField!
+    @IBOutlet weak var currencySegment: UISegmentedControl!
+    @IBOutlet weak var convertedLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func convertTapped(_ sender: Any) {
+        let rate = Double(rateTextField.text!)!
+        let amount = Double(amountTextField.text!)!
+        let converted = amount * rate
+        if currencySegment.selectedSegmentIndex == 0 {
+            convertedLabel.text = "₹ \(String(converted))"
+        }else{
+            convertedLabel.text = "$\(String(converted))"
+        }
+        
+        
+    }
+    
 }
+
 
